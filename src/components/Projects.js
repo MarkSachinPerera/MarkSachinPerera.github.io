9c7724c6a51877cea750f23ed8d773c6c892101f
MarkSachinPerera <marksachin12@gmail.com>
2021-01-01 15:43:14 -0700

    magic mirror

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,8 @@ import image1 from '../assets/297Images/img1.jpg'
 import image6 from '../assets/297Images/img6.jpg'
 import image7 from '../assets/297Images/img7.jpg'
 import Container from 'react-bootstrap/Container'
+import magic1 from '../assets/Screenshot_20200815-223203_Instagram.jpg'
+import magic2 from '../assets/20200606_220442.jpg'
 
 function Projects() {
     return (
@@ -11,12 +13,14 @@ function Projects() {
         <Container fluid="md">
             <h2>GIS Project</h2>
             <p><small>Worked in a team of 3. We used C++ and the OpenStreetMaps library for this.</small></p>
-            <Carousel className="w-80">
+            <br/>
+
+            <Carousel className="w-auto h-auto">
                 <Carousel.Item >
                     <img
                         src={image0}
                         alt=""
-                        className="w-100"
+                        className="w-100 h-auto"
                     />
                     <Carousel.Caption className="carousel-subtext">
                         <h3 className="carousel-text">Searching for a street, POI, or an intersection</h3>
@@ -25,7 +29,7 @@ function Projects() {
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="w-100"
+                        className="w-100 h-auto"
                         src={image1}
                         alt=""
                     />
@@ -37,7 +41,7 @@ function Projects() {
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="w-100"
+                        className="w-100 h-auto"
                         src={image6}
                         alt=""
                     />
@@ -49,7 +53,7 @@ function Projects() {
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="w-100"
+                        className="w-100 h-auto"
                         src={image7}
                         alt=""
                     />
@@ -59,6 +63,31 @@ function Projects() {
                         <h5 className="carousel-text" >Demo of our navigation from point A to B. The directions were compiled after running Dijkstar.</h5> 
                     </Carousel.Caption>
                 </Carousel.Item>
+
+            </Carousel>
+
+            <br/>
+
+            <h2>Magic Mirror Build</h2>
+            <p><small>Made a magic mirror from the hardware of an old laptop.</small></p>
+
+            <Carousel className="w-auto">
+                <Carousel.Item >
+                    <img
+                        src={magic1}
+                        alt=""
+                        className="w-50 h-75"
+                    />
+
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="w-auto"
+                        src={magic2}
+                        alt=""
+                    />
+                </Carousel.Item>
+
 
             </Carousel>
 
