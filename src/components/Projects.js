46b1ca7beb46fa2664b4361de7a2594fb2d2c4cb
MarkSachinPerera <marksachin12@gmail.com>
2021-01-01 17:07:10 -0700

    theme redone

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,15 +12,29 @@ import Button from 'react-bootstrap/Button'
 import chef1 from '../assets/chef1.jpg'
 import chef2 from '../assets/chef2.jpg'
 import chef3 from '../assets/chef3.jpg'
+import Card from 'react-bootstrap/Card'
 
 function Projects() {
     return (
 
         <Container fluid="md">
-            <h2>GIS Project</h2>
-            <p><small>I worked in a team of 3. We used C++ and the OpenStreetMaps library for this. Our final product was able to search streets, intersections, and POI. With a Dijkstra algorithm we were
-                also able to give the user directions to travel between any two points in the map.</small></p>
-            <br />
+
+            <Container fluid>
+                <Card fluid className="text-center">
+                    <Card.Header>&#47;&#47; GIS Project </Card.Header>
+                        <Card.Body>
+                            <h2>&#47;*</h2>
+                            <Card.Text>
+            
+                            I worked in a team of 3. We used C++ and the OpenStreetMaps library for this. Our final product was able to search streets, intersections, and POI. With a Dijkstra algorithm we were
+                            also able to give the user directions to travel between any two points in the map.
+        
+                            </Card.Text>
+                            <h2>*&#47;</h2>
+                        </Card.Body>
+                </Card>
+            </Container>
+            <br/>
 
             <Carousel className="w-75 h-75 center">
                 <Carousel.Item >
@@ -75,11 +89,24 @@ function Projects() {
 
             <br />
 
-            <h2>Magic Mirror Build</h2>
-            <p><small>I stripped an old laptop of all non-essistenial components to create a device similar to that of a raspeberry pie. No keyboard,
-            no track pad, no LCD screen. Rewired the wifi card to the main body and booted up Lubuntu with the software to run magicmirror.
-            Then I connected the setup to my TV with CEC. The magicmirror modules allowed me to get quick news updates as well as display
-                my calendar to keep track of my day.</small></p>
+            <Container fluid>
+                <Card fluid className="text-center">
+                    <Card.Header>&#47;&#47; Magic Mirror Build </Card.Header>
+                        <Card.Body>
+                            <h2>&#47;*</h2>
+                            <Card.Text>
+            
+                            I stripped an old laptop of all non-essistenial components to create a device similar to that of a raspeberry pie. No keyboard,
+                             no track pad, no LCD screen. Rewired the wifi card to the main body and booted up Lubuntu with the software to run magicmirror.
+                             Then I connected the setup to my TV with CEC. The magicmirror modules allowed me to get quick news updates as well as display
+                              my calendar to keep track of my day.
+        
+                            </Card.Text>
+                            <h2>*&#47;</h2>
+                        </Card.Body>
+                </Card>
+            </Container>
+            <br/>
 
             <Carousel className="w-auto">
                 <Carousel.Item >
@@ -102,20 +129,36 @@ function Projects() {
 
             <br />
 
-            <h2>Tic-Tac-Toe Game</h2>
-            <p><small>In the Summer of 2018 I used java to
-            release my very own Tic Tac Toe game
-            on the android application store. The game has a built in AI for its single player game mode. The AI
-            can be
-            disabled and enabled
-            by the user as needed. My goal was to provide a formidable opponent that will take multiple tries to
-            out
-            maneuver and defeat.
-            In addition, the game provides the user with directions indicating the next player and prompts the user
-            when the game ends. When
-            there is a win or loss the game has a designated score board that keeps track of which player is
-                winning.</small></p>
-            <Button className="btn-outline-secondary" href="https://play.google.com/store/apps/details?id=com.markperera.tictactoe">Go to the Playstore </Button>{' '}
+            <Container fluid>
+                <Card fluid className="text-center">
+                    <Card.Header>&#47;&#47; Tic-Tac-Toe Game </Card.Header>
+                        <Card.Body>
+                            <h2>&#47;*</h2>
+                            <Card.Text>
+            
+                            In the Summer of 2018 I used java to
+                            release my very own Tic Tac Toe game
+                            on the android application store. The game has a built in AI for its single player game mode. The AI
+                            can be
+                            disabled and enabled
+                            by the user as needed. My goal was to provide a formidable opponent that will take multiple tries to
+                            out
+                            maneuver and defeat.
+                            In addition, the game provides the user with directions indicating the next player and prompts the user
+                            when the game ends. When
+                            there is a win or loss the game has a designated score board that keeps track of which player is
+                              winning.
+        
+                            </Card.Text>
+                            <h2>*&#47;</h2>
+                            <Button className="btn-outline-secondary" href="https://play.google.com/store/apps/details?id=com.markperera.tictactoe">Go to the Playstore </Button>{' '}
+                        </Card.Body>
+                </Card>
+            </Container>
+
+            <br/>
+
+            
 
             <Carousel className="w-auto">
                 <Carousel.Item >
@@ -138,15 +181,28 @@ function Projects() {
 
             <br/>
 
-            <h2>Chef-co Pilot</h2>
-            <p><small>This was a class project. I worked in a group of 3 people. Our task was to interview potential stakeholders such as students and grocery store workers
-                and create a web application that can help students to better manage their time.
+            <Container fluid>
+                <Card fluid className="text-center">
+                    <Card.Header>&#47;&#47; Chef-co Pilot </Card.Header>
+                        <Card.Body>
+                            <h2>&#47;*</h2>
+                            <Card.Text>
+            
+                            This was a class project. I worked in a group of 3 people. Our task was to interview potential stakeholders such as students and grocery store workers
+                             and create a web application that can help students to better manage their time.
                 
-                I was the a frontend developer for this project and we used ReactJs to build the user interface. This is hosted on herko. Due to unseen circumstances the backend was never fully completed.
-                Due to this, the site does not have the fully polised finish I was hoping.
+                             I was the a frontend developer for this project and we used ReactJs to build the user interface. This is hosted on herko. Due to unseen circumstances the backend was never fully completed.
+                             Due to this, the site does not have the fully polised finish I was hoping.
                 
-                (give the site some time to load).</small></p>
-            <Button className="btn-outline-secondary" href="https://chef-copilot.herokuapp.com/Recipes">Visit the site </Button>{' '}
+                            (give the site some time to load).
+        
+                            </Card.Text>
+                            <h2>*&#47;</h2>
+                            <Button className="btn-outline-secondary" href="https://chef-copilot.herokuapp.com/Recipes">Visit the site </Button>{' '}
+                        </Card.Body>
+                </Card>
+            </Container>
+            
             <br/>
             <br/>
             <Carousel className="w-100 h-100">
@@ -177,12 +233,25 @@ function Projects() {
 
             <br/>
 
-            <h2>Search Engine</h2>
-            <p><small> As a part of my programming languages course, my teammate and I designed a search engine that we
-                deployed through AWS.
-                Our primary languages included HTML, Redis, CSS, and python. My main focus was on the backend. I implemented a crawler and page
-                ranker that ran at startup to build our databases that provided information to the frontend.</small></p>
-            <Button className="btn-outline-secondary" href="https://github.com/MarkSachinPerera/CSC326">Visit the Repository </Button>{' '}
+            <Container fluid>
+                <Card fluid className="text-center">
+                    <Card.Header>&#47;&#47; Search Engine</Card.Header>
+                        <Card.Body>
+                            <h2>&#47;*</h2>
+                            <Card.Text>
+            
+                            As a part of my programming languages course, my teammate and I designed a search engine that we
+                             deployed through AWS.
+                             Our primary languages included HTML, Redis, CSS, and python. My main focus was on the backend. I implemented a crawler and page
+                            ranker that ran at startup to build our databases that provided information to the frontend.
+        
+                            </Card.Text>
+                            <h2>*&#47;</h2>
+                            <Button className="btn-outline-secondary" href="https://github.com/MarkSachinPerera/CSC326">Visit the Repository </Button>{' '}
+                        </Card.Body>
+                </Card>
+            </Container>
+            
             <br/>
 
         </Container>
