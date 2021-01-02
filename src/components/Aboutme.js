946b37ff081cf9657632947f2d2eea67943bb5d3
MarkSachinPerera <marksachin12@gmail.com>
2021-01-01 17:58:43 -0700

    skills cleanedup

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -64,23 +64,32 @@ function AboutMe(params) {
             <br/>
             <Jumbotron>
                 <Container>
-                    <h1 className="text-center">Skills</h1>
-                    <Row>
-                        {skillList.map((skill) => (
-                            <Col xs={2} md={2} className="p-3">
-                                <Container>
-                                    <i className={skill} style={{ fontSize: "300%", alignContent: "center" }}></i>
-                                </Container>
-                            </Col>
-                        ))}
-                        {skillList2.map((skill) => (
-                            <Col xs={2} md={2} className="p-3">
-                                <Container>
-                                    <Image src={skill} className="icon"></Image>
-                                </Container>
-                            </Col>
-                        ))}
-                    </Row>
+                    
+                    
+                        <Card fluid className="text-center">
+                        <Card.Header><h1 className="text-center">Skills</h1> </Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                <Row>
+                                    {skillList.map((skill) => (
+                                        <Col xs={2} md={2} className="p-3">
+                                            <Container>
+                                                <i className={skill} style={{ fontSize: "300%", alignContent: "center" }}></i>
+                                            </Container>
+                                        </Col>
+                                    ))}
+                                    {skillList2.map((skill) => (
+                                        <Col xs={2} md={2} className="p-3">
+                                            <Container>
+                                                <Image src={skill} className="icon"></Image>
+                                            </Container>
+                                        </Col>
+                                    ))}
+                                    </Row>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    
                 </Container>
             </Jumbotron>
             <Jumbotron>
